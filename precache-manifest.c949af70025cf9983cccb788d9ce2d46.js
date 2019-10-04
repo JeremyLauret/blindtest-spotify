7a28947fc2819bfb73a70592a93ae858fc108253
JeremyLauret <jeremyl@theodo.fr>
2019-10-04 14:42:23 +0200

    Updates

---
+++ precache-manifest.c949af70025cf9983cccb788d9ce2d46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "e4c39db1f6e672589f3b",
+    "url": "/blindtest-spotify/static/js/main.e4c39db1.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "e4c39db1f6e672589f3b",
+    "url": "/blindtest-spotify/static/css/main.ecc4eca5.chunk.css"
+  },
+  {
+    "revision": "fff577b8e90d49408f5677595a5505d1",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
