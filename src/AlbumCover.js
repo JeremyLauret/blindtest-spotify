fb5ae96d20efd129ab24ec0b2ba03a9b51cfaf5a
JeremyLauret <jeremy.lauret24@gmail.com>
2019-10-10 23:57:09 +0200

    Final state

---
+++ src/AlbumCover.js
@@ -0,0 +1,21 @@
+/*global swal*/
+
+import React, { Component } from "react";
+import Sound from "react-sound";
+
+class AlbumCover extends Component {
+  render() {
+    const albumImages = this.props.track.album.images;
+    const previewUrl = this.props.track.preview_url;
+    return albumImages.length > 0 ? (
+      <div>
+        <img src={this.props.track.album.images[1].url}></img>
+        <Sound url={previewUrl} playStatus={Sound.status.PLAYING} />
+      </div>
+    ) : (
+      <p>No image was found for this track!</p>
+    );
+  }
+}
+
+export default AlbumCover;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 /*global swal*/
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import loading from './loading.svg';
-import './App.css';
-import Sound from 'react-sound';
-import Button from './Button';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import loading from "./loading.svg";
+import "./App.css";
+import Button from "./Button";
+import AlbumCover from "./AlbumCover";
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+const apiToken =
+  "BQC0jZshX7RTxZ-IIseLkUAONI9K0HxCAj9Jx0X5iTlrtXl0mqAEdFEu2A9yW4yEIIc5wsBzoQO2wo6jdttFWpmm_TGtErIgQT9vIPDRoCLkrXC4s7QZWfckarDbyBA4-FU7VPfb2ShlTIEEKwWhWELLqbe7Sp_LoIRB_3o1r9AO";
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -29,22 +30,92 @@ function getRandomNumber(x) {
 }
 
 class App extends Component {
-
   constructor() {
     super();
+    this.state = {
+      tracks: [],
+      currentTrack: {},
+      songsLoaded: false,
+      timeout: null
+    };
+  }
+
+  loadAnotherTrack() {
+    clearTimeout(this.state.timeout);
+    this.setState({
+      currentTrack: this.state.tracks[getRandomNumber(this.state.tracks.length)].track,
+      timeout: setTimeout(this.loadAnotherTrack.bind(this), 10000)
+    });
+  }
+
+  checkAnswer(trackId) {
+    trackId === this.state.currentTrack.id
+      ? swal("Bravo !", "Tu es svelte et élancé", "success").then(
+          this.loadAnotherTrack.bind(this)
+        )
+      : swal("Non", "Tu as le cancer et tu es adopté", "error");
+  }
+
+  componentDidMount() {
+    fetch("https://api.spotify.com/v1/me/tracks", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + apiToken
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          tracks: data.items,
+          currentTrack: data.items[getRandomNumber(data.items.length)].track,
+          songsLoaded: true,
+          timeout: setTimeout(this.loadAnotherTrack.bind(this), 10000)
+        });
+      });
   }
 
   render() {
+    let track1 = {};
+    let track2 = {};
+    let track3 = {};
+
+    if (this.state.songsLoaded) {
+      track1 = this.state.currentTrack;
+      while (!track2.id || this.state.tracks[track2.id].track.id === track1.id) {
+        track2.id = getRandomNumber(this.state.tracks.length);
+      }
+      track2 = this.state.tracks[track2.id].track;
+      while (
+        !track3.id ||
+        this.state.tracks[track3.id].track.id === track1.id ||
+        this.state.tracks[track3.id].track.id === track2.id
+      ) {
+        track3.id = getRandomNumber(this.state.tracks.length);
+      }
+      track3 = this.state.tracks[track3.id].track;
+    }
+    let tracks = [track1, track2, track3];
+    tracks = shuffleArray(tracks);
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Bienvenue sur le Blindtest</h1>
         </header>
         <div className="App-images">
-          <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+          {this.state.songsLoaded ? (
+            <AlbumCover track={track1} />
+          ) : (
+            <img src={loading} className="App-logo" alt="loading" />
+          )}
         </div>
         <div className="App-buttons">
+          {tracks.map(track => (
+            <Button onClick={() => this.checkAnswer(track.id)}>
+              {track.name}
+            </Button>
+          ))}
         </div>
       </div>
     );
